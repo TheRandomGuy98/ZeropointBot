6ae05635f37b05bf45582108fa08cc196b2771eb
Damien Vesper <ldamienvesper@gmail.com>
2021-04-24 15:07:09 -0500

    get me some linus tech

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,10 +2,12 @@ import { Client } from '../index';
 import { log, logHeader } from '../utils/log';
 
 import { refreshActivity } from '../utils/refreshActivity';
+import playLinus from '../utils/playLinus';
 
 export default async (client: Client) => {
     log(`green`, `${client.user.tag} has started, with ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`);
     logHeader();
 
     refreshActivity(client);
+    playLinus(client);
 };
---
+++ src/utils/playLinus.ts
@@ -0,0 +1,15 @@
+import * as Discord from 'discord.js';
+import { Client } from '../index';
+
+import * as ytdl from 'ytdl-core';
+
+const getFreeTechTips = (connection: Discord.VoiceConnection) => connection.play(ytdl(`https://www.youtube.com/watch?v=PKfxmFU3lWY`));
+
+const playLinus = async (client: Client) => {
+    const linusChannel: Discord.Channel = await client.channels.fetch(`835599503263793232`);
+
+    const freeTechTips = await (linusChannel as Discord.VoiceChannel).join();
+    getFreeTechTips(freeTechTips).on(`end`, () => getFreeTechTips(freeTechTips));
+};
+
+export default playLinus;
