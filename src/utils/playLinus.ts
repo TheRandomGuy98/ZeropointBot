4006e46cfb1128f00dcb73d7b941a69520dffa10
Damien Vesper <ldamienvesper@gmail.com>
2021-07-27 22:45:31 -0500

    doctype linustechtips

--- src/utils/playLinus.ts
+++ src/utils/playLinus.ts
@@ -3,8 +3,17 @@ import { Client } from '../types/discord';
 
 import ytdl from 'ytdl-core';
 
+/**
+ * Get some free tech tips.
+ * @param connection The connection to connect to.
+ * @returns The stream of tech tips.
+ */
 const getFreeTechTips = (connection: Discord.VoiceConnection) => connection.play(ytdl(`https://www.youtube.com/watch?v=PKfxmFU3lWY`));
 
+/**
+ * Play a LinusTechTips video.
+ * @param client The client to play the video with.
+ */
 const playLinus = async (client: Client) => {
     const linusChannel: Discord.Channel = await client.channels.fetch(`840081062314639370`);
 
