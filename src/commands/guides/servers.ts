9bfb949bcc94a86497ce486c1b101d17cddfb420
Damien Vesper <ldamienvesper@gmail.com>
2021-07-29 20:46:57 -0500

    add a servers command

---
+++ src/commands/guides/servers.ts
@@ -0,0 +1,29 @@
+import config from '../../../config/config';
+
+import * as Discord from 'discord.js-light';
+import { Client, CommandConfig } from '../../types/discord';
+
+const cmd: CommandConfig = {
+    desc: `View all servers.`,
+    category: `guides`
+};
+
+const run = async (client: Client, message: Discord.Message, args: string[]) => {
+    const serverEmbed: Discord.MessageEmbed = new Discord.MessageEmbed()
+        .setAuthor(`Servers`, message.guild.iconURL(), `https://alru.ga/servers`)
+        .addField(`Garry's Mod`, `
+            Family Guy DarkRP - [65.21.242.45:9000](https://alru.ga/servers)
+            Among Us - [207.225.26.215:65436](https://alru.ga/servers)
+            CityRP - [207.225.26.215:65437](https://alru.ga/servers)
+        `)
+        .addField(`Minecraft`, `AlruMC Network - [mc.alru.ga](https://alru.ga/servers)`)
+        .setTimestamp(new Date())
+        .setFooter(config.footer);
+
+    message.channel.send(serverEmbed);
+};
+
+export {
+    cmd,
+    run
+};
