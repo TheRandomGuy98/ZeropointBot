d92f3f028bdb0c14c34c2fafcc8f1de6b2754b18
Damien Vesper <ldamienvesper@gmail.com>
2021-08-01 22:10:30 -0500

    coelus wants a breakfast burrito? whaaat?

---
+++ src/commands/misc/burrito.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import * as Discord from 'discord.js-light';
+import { Client, CommandConfig } from '../../types/discord';
+
+const cmd: CommandConfig = {
+    desc: `Coelus' favorite burrito.`,
+    aliases: [`breakfastburrito`, `coelus`],
+    category: `misc`
+};
+
+const run = async (client: Client, message: Discord.Message, args: string[]) => {
+    const m = `${message.author} »`;
+
+    fs.readdir(path.resolve(__dirname, `../../../assets/img/burrito`), (err, files) => {
+        if (err) throw err;
+        const rng = Math.floor(Math.random() * files.length) + 1;
+
+        message.channel.send({
+            files: [path.resolve(__dirname, `../../../assets/img/burrito/${rng}.png`)],
+            content: `${m} Coelus wants a breakfast burrito? Whaaat?!`
+        });
+    });
+};
+
+export {
+    cmd,
+    run
+};
