de9aef44773fcf2013dab22728336d441a72df7b
Damien Vesper <ldamienvesper@gmail.com>
2021-08-02 11:11:04 -0500

    fix queue

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -18,24 +18,24 @@ const run = async (client: Client, message: Discord.Message, args: string[]) => 
     if (!message.member.voice.channel) return message.channel.send(`${m} You must be in a voice channel to use this!`);
     if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${m} You must be in the same voice channel as me to use this!`);
 
-    if (!client.player.isPlaying(message)) message.channel.send(`${m} There is no song currently playing!`);
-
     const queue = client.player.getQueue(message);
+    if (!queue || queue.tracks.length === 0) return message.channel.send(`${m} There are no songs in the queue!`);
 
     const queueEmbed = new FieldsEmbed()
-        .setArray(queue.tracks.map((tracks, i) => `${i === 0 ? `\`Current\`` : `${i + 1}`}: **${cleanse(tracks.author)} - ${cleanse(tracks.title)}**`))
+        .setArray(queue.tracks)
+        .formatField(`Songs`, (i: any) => `**${queue.tracks.indexOf(i) === 0 ? `Current` : `${queue.tracks.indexOf(i)}`}:** [${cleanse(i.title)}](${i.url}) ${queue.tracks.indexOf(i) === 0 ? `\n` : ``}`)
         .setAuthorizedUsers([message.author.id])
         .setChannel((message.channel as Discord.TextChannel))
         .setElementsPerPage(10)
         .setPage(1)
         .setPageIndicator(true)
-        .setDeleteOnTimeout(true)
         .setDisabledNavigationEmojis([`delete`]);
-
     queueEmbed.embed
         .setAuthor(`Server Queue`, message.guild.iconURL())
         .setTimestamp(new Date())
         .setFooter(config.footer);
+
+    await queueEmbed.build();
 
     message.reply(queueEmbed);
 };
