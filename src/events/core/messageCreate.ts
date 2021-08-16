ca3ad3e8b41d132cef4c0b4700b9d7f5bcc265ea
Damien Vesper <ldamienvesper@gmail.com>
2021-08-16 18:53:38 -0500

    messageCreate: cleanup

--- src/events/core/messageCreate.ts
+++ src/events/core/messageCreate.ts
@@ -1,12 +1,9 @@
 import * as Discord from 'discord.js';
 import { Client } from '../../typings/discord';
 
-import config from '../../../config/config';
 import log from '../../utils/log';
 
 export default async (client: Client, message: Discord.Message) => {
-    const m = `${message.author} »`;
-
     // Botception.
     if (message.author.bot || message.channel.type === `DM`) return;
 
@@ -15,25 +12,5 @@ export default async (client: Client, message: Discord.Message) => {
         message.delete();
         message.author.send(`We do not have S&box keys. The video linking it to our Discord was not uploaded by us, and we have no relation to it.\nPlease, when you have the chance, report said video so it gets removed.`)
             .catch(() => log(`red`, `Failed to DM S&box warning to ${message.author.tag}`));
-    }
-
-    // Prefix handling.
-    if (message.content.slice(0, config.prefix.length).toString().toLowerCase() !== config.prefix) return;
-
-    // Parse arguments and command.
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    // Grab the command from the handler.
-    const cmd = client.commands.get(command) ||
-        client.commands.get([...client.commands.keys()][[...client.commands.values()].indexOf([...client.commands.values()].find(cmd => cmd.config.aliases?.includes(command)))]);
-
-    if (!cmd) return;
-
-    if ((cmd.config.usage) && args.length < (cmd.config.usage.split(`<`).length) - 1) return message.channel.send(`${m} Proper usage is \`${config.prefix + command} ${cmd.config.usage}\`.`);
-    else {
-        // Execute the command.
-        log(`magenta`, `${message.author.tag} [${message.author.id}] ran command ${command} in ${message.guild.name}.`);
-        cmd.run(client, message, args);
     }
 };
